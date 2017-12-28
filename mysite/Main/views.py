739a0268f116cb61a8ad98e85f8f98ec14a339db
Ngahu <jamaalaraheem@gmail.com>
2017-12-29 01:11:52 +0300

    creating of the utils file that is going to be generating the unique slugs .This is to makesure that every post gets a unique slug

--- mysite/Main/views.py
+++ mysite/Main/views.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+
+class PostListView(ListView):
+    pass
+
+
+
+
+class PostDetailView(DetailView):
+    pass
+
+
+class PostCreateView(CreateView):
+    pass
+
+
+
+class PostUpdateView(UpdateView):
+    pass
+
+
+class PostDeleteView(DeleteView):
+    pass
+
+

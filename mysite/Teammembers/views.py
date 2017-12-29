c98d68b5bd45ab4ea290b6f5e9ac1981180bc8b8
Ngahu <jamaalaraheem@gmail.com>
2017-12-29 17:04:01 +0300

    creating ofthe  vies for the tem members app

--- mysite/Teammembers/views.py
+++ mysite/Teammembers/views.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
 from django.shortcuts import render
-
+from .models import Team_Meamber
 # Create your views here.
+
+
+class Team_MeamberListView(ListView):
+    pass
+
+
+
+class Team_MeamberCreateView(CreateView):
+    pass
+
+
+class Team_MeamberListView(ListView):
+    pass
+
+
+class Team_MeamberUpdateView(UpdateView):
+    pass
+
+
+class Team_MeamberDetailView(DetailView):
+    pass
+
+
+class Team_MeamberDeleteView(DeleteView):
+    pass

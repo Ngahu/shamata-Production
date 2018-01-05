211044e4c15ad1aa5c84242f12e21863e9c28b03
Ngahu <jamaalaraheem@gmail.com>
2018-01-05 06:36:02 +0300

    creating of the comments reply that is allowing any user to reply to any comment

--- mysite/Comments/models.py
+++ mysite/Comments/models.py
@@ -11,10 +11,14 @@ User = settings.AUTH_USER_MODEL
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager,self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self,instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)# instance.__class__ a way to get python's class
         obj_id = instance.id
-        qs = super(CommentManager,self).filter(content_type=content_type,object_id=obj_id)
+        qs = super(CommentManager,self).filter(content_type=content_type,object_id=obj_id).filter(parent=None)
         return qs 
 
 

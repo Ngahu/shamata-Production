1f4f8b3dbfef32d6d26dc93d96fae96731d704b5
Ngahu <jamaalaraheem@gmail.com>
2018-01-07 00:39:56 +0300

    creatingof the delete button for the comments app

--- mysite/Comments/views.py
+++ mysite/Comments/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
+from .forms import  CommentForm
+from .models import Comment
+
+def  comment_delete(request,id):
+    obj = get_object_or_404(Comment,id=id)
+    context = {
+        "object":obj
+    }
+    template_name = 'Main/confirm_delete.html'
+    return render(request,template_name,context)

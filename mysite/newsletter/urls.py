061cc0a1c15a9757ff4a906cd828bd9b30c42882
Ngahu <jamaalaraheem@gmail.com>
2018-01-07 23:42:17 +0300

    creating of the subscribe view fr the newsletter app

--- mysite/newsletter/urls.py
+++ mysite/newsletter/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .views import (
-    my_email_list
+    my_email_list,
+    SubscribeView
 )
 
 urlpatterns = [ 
-     url(r'^my_email_list/$',my_email_list,name='my_email_list'),
+    url(r'^my_email_list/$',my_email_list,name='my_email_list'),
+    url(r'^subscribe/$',SubscribeView.as_view(),name='subscribe'),
 ]
--- mysite/newsletter/views.py
+++ mysite/newsletter/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .models import Subscribe
 from .forms import SubscriptionForm
-# Create your views here.
+from django.views.generic import  FormView
 from .models import Subscribe
 
 #This view will be displaying a list of the emails that have subscribed to the news letter section 
@@ -16,3 +16,8 @@ def my_email_list(request):
     template_name = 'newsletter/newsletter_list.html'
     return render(request, template_name, context)
 
+
+
+class SubscribeView(FormView):
+    template_name = 'newsletter/subscribe.html'
+    form_class = SubscriptionForm

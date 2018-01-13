7c47abf8cf3724f2c017cf3393ca54f8bd6bc4eb
Ngahu <jamaalaraheem@gmail.com>
2018-01-13 14:43:31 +0300

    creating of the Testimony model.This will be used to correct testimonies only for registered users

--- mysite/newsletter/admin.py
+++ mysite/newsletter/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Subscribe
+from .models import Subscribe,Testimony
 # Register your models here.
 
 admin.site.register(Subscribe)
+admin.site.register(Testimony)
--- mysite/newsletter/models.py
+++ mysite/newsletter/models.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
+
 # Create your models here.
+
 class Subscribe(models.Model):
     name =models.CharField(max_length=50)
     email = models.EmailField()
@@ -18,3 +22,23 @@ class Subscribe(models.Model):
 
     def get_delete_url(self):
         return reverse("newsletter:my_email_list",kwargs={"id": self.id})
+
+
+
+
+
+
+class Testimony(models.Model):
+    owner      = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject    = models.CharField(max_length=100)
+    testimony  = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+    def __unicode__(self):
+        return str(self.owner   )
+
+    class  Meta:
+        ordering = ["-timestamp"]
+        verbose_name = "Testimony"
+        verbose_name_plural = "Testimonies"

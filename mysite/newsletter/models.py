d55959e0af9413c0fc53f9372310d9b372c5dbd9
Ngahu <jamaalaraheem@gmail.com>
2018-01-13 14:30:05 +0300

    creating of the newsletter html page that is going to be used in the main site

--- mysite/newsletter/models.py
+++ mysite/newsletter/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Subscribe(models.Model):
+    name =models.CharField(max_length=50)
     email = models.EmailField()
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
